85c9a7dfaf6d4d2be028bd1b1c992ce186a37574
Derek Croft <derek@derekcroft.com>
2012-04-07 13:21:48 -0400

    Remove static assets path from engine

--- lib/zable/engine.rb
+++ lib/zable/engine.rb
@@ -5,7 +5,6 @@ require 'action_controller'
 module Zable
   class Engine < Rails::Engine
     initializer "static assets" do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
     end
   end
 end
